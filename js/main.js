7c02c3cefe8152fa2111466e55637d668684db9b
Josie Lee <josie.digitalad@gmail.com>
2026-06-25 17:08:29 +0800

    Load first memory photo

--- js/main.js
+++ js/main.js
@@ -3,8 +3,10 @@ const params = new URLSearchParams(window.location.search);
 const user = params.get("user") || "jim";
 
 const letterPath = `data/${user}/letter.txt`;
+const imagePath = `data/${user}/01.jpg`;
 
 console.log(letterPath);
+console.log(imagePath);
 
 fetch(letterPath)
     .then(response => {
@@ -13,9 +15,13 @@ fetch(letterPath)
         }
         return response.text();
     })
-    .then(text => {
-        console.log(text);
-    })
+   .then(text => {
+
+    const letterContent = document.getElementById("letterContent");
+
+    letterContent.innerText = text;
+
+})
     .catch(error => {
         console.log(error.message);
     });
@@ -29,10 +35,17 @@ const letterPage = document.getElementById("letter");
 
 const startBtn = document.getElementById("startBtn");
 const nextBtn = document.getElementById("nextBtn");
+const mediaContainer = document.getElementById("mediaContainer");
 
 startBtn.addEventListener("click", () => {
+
     welcomePage.style.display = "none";
     galleryPage.style.display = "block";
+
+    mediaContainer.innerHTML = `
+        <img src="${imagePath}" alt="Memory">
+    `;
+
 });
 
 nextBtn.addEventListener("click", () => {
