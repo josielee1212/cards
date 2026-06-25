d94f143426be8c565669b3f329548d4b3dc18f7c
Josie Lee <josie.digitalad@gmail.com>
2026-06-25 15:52:39 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
+const envelope=document.querySelector(".envelope");
 
+envelope.addEventListener("click",()=>{
+
+    envelope.style.transform="scale(.95)";
+
+    setTimeout(()=>{
+
+        envelope.style.transform="scale(1)";
+
+    },150);
+
+});
